33846b3bc58c0ac6eb32a9d5004ec4d4f3f5a8cd
tomaswyz <jackywyz@126.com>
2013-10-29 22:21:33 +0800

    Fixed the dependency

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -4,6 +4,8 @@ scalaVersion :="2.10.3"
 seq(webSettings :_*)
 
 libraryDependencies ++= Seq("org.mortbay.jetty" % "jetty" % "6.1.26" % "container",
-"ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime"
+"ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
+"javax.servlet" % "servlet-api" % "2.5" % "provided",
+"org.slf4j" % "slf4j-api" % "1.7.5"
 )
 
--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,3 +1,27 @@
-class Stub {
-  // got any helpful boilerplate for your users?
+package com.test
+import javax.servlet.http._
+import javax.servlet._
+import org.slf4j.LoggerFactory
+
+trait ConfigHttp{self:HttpServlet =>
+ import java.io.PrintWriter
+ def config(req:HttpServletRequest,res:HttpServletResponse):PrintWriter = {
+   req.setCharacterEncoding("UTF-8")
+   res.setContentType("text/plain; charset=UTF-8")
+   res.getWriter()
+  }
+ def resp(out:PrintWriter,res:String="") = {
+   out.write(res) 
+   out.close()
+ }
+ override def doPost(req:HttpServletRequest,res:HttpServletResponse)={
+   self.doGet(req,res)
+ }
+}
+
+class Stub extends  HttpServlet with ConfigHttp{
+  def logger = LoggerFactory.getLogger("web")
+ override def doGet(req:HttpServletRequest,res:HttpServletResponse)={ 
+   logger.info("link request")
+ }
 }
